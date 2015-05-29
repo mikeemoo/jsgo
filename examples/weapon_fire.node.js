669d4d64c72b2ad74441fcc1fbeb783e062c97c2
Mike <mike@crimson.gg>
2015-05-29 21:52:01 +0100

    fixed entity/player matching

--- examples/weapon_fire.node.js
+++ examples/weapon_fire.node.js
@@ -1,17 +1,15 @@
 var fs = require('fs');
 var jsgo = require('../lib/jsgo');
 
-fs.readFile('../demo.dem', function(err, data) {
+fs.readFile('1668_virtus-pro-fnatic_de_cbble.dem', function(err, data) {
+
+    var fires = 0;
 
     var myDemo = new jsgo.Demo().
-    	on('game.weapon_fire', function(event) {
-
-            var player = event.player;
-            var position = player.getPosition();
-
-            console.log(player.getName() + ' used weapon ' +
-            			event.weapon + ' at ' + position.x + ', ' + position.y + ', ' + position.z);
-        }).
-        parse(data);
+    on('game.weapon_fire', function(event) {
+      console.log(Object.keys(event.player.getActiveWeapon().classInfo.dataTable));
+      //console.log(event.player.getActiveWeapon().getValue('m_hOwner') & 0x7FF);
+    }).
+    parse(data);
 
 });
